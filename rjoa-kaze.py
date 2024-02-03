0300f1f9053e845398a3cdc4205baa9991a79c9b
anaflightsim <66984894+anaflightsim@users.noreply.github.com>
2024-02-03 04:51:06 -0500

    Version 1.1 Update

---
+++ rjoa-kaze.py
@@ -0,0 +1,25 @@
+﻿msfs_mode = 1
+
+parkings = {
+    GATE : {
+        None : (),
+        2 : (CustomizedName("Apron Gate|Spot #§ [Domestic]"), ),
+        3 : (CustomizedName("Apron Gate|Spot #§ [Domestic]"), ),
+        5 : (CustomizedName("Apron Gate|Spot #§ [Domestic]"), ),
+        6 : (CustomizedName("Apron Gate|Spot #§ [DOM/INTL]"), ),
+        7 : (CustomizedName("Apron Gate|Spot #§ [INTL]"), ),
+        8 : (CustomizedName("Apron Gate|Spot #§ [INTL]"), ),
+
+    },
+    PARKING : {
+        None : (),
+        1 : (CustomizedName("Apron Stand|Spot #§"), ),
+        9 : (CustomizedName("Apron Stand|Spot #§"), ),
+        10 : (CustomizedName("Apron Stand|Spot #§"), ),
+        11 : (CustomizedName("Sub Apron|3"), ),
+        12 : (CustomizedName("Sub Apron|2"), ),
+        13 : (CustomizedName("Sub Apron|1"), ),
+        14 : (CustomizedName("Heli Apron|WEST"), ),
+        15 : (CustomizedName("Heli Apron|EAST"), ),
+    }
+}
