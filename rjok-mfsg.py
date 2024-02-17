62de571ebad2b3707554af07dd1fbc83e00b0a83
Rokumaru <73397638+Rokumaru-JPN@users.noreply.github.com>
2024-02-17 13:23:21 +0900

    Version 1.0 upload

---
+++ rjok-mfsg.py
@@ -0,0 +1,36 @@
+﻿msfs_mode = 1
+
+
+parkings = {
+    GATE :{
+        None : (),
+        0 : (CustomizedName("Apron Stand|Spot #§"), ),
+        1 : (CustomizedName("Apron Stand|Spot #§"), ),
+        2 : (CustomizedName("Apron Gate|Spot #§ [JAL/JJP/FDA]"), ),
+        3 : (CustomizedName("Apron Gate|Spot #§ [JAL/JJP/FDA]"), ),
+        4 : (CustomizedName("Apron Gate|Spot #§ [ANA]"), ),
+        5 : (CustomizedName("Apron Stand|Spot #§"), ),
+        6 : (CustomizedName("Spot 6/GA Parking|A"), ),
+        7 : (CustomizedName("Spot 6/GA Parking|B"), ),
+        8 : (CustomizedName("Spot 6/GA Parking|C"), ),
+        9 : (CustomizedName("Spot 6/GA Parking|D"), ),
+        10 : (CustomizedName("Spot 6/GA Parking|E"), ),
+        11 : (CustomizedName("Spot 6/GA Parking|F"), ),
+        12 : (CustomizedName("Spot 6/GA Parking|G"), ),
+        13 : (CustomizedName("Spot 6/GA Parking|H"), ),
+        14 : (CustomizedName("Spot 6/GA Parking|I"), ),
+    },
+    PARKING :{
+        None : (),
+        6 : (CustomizedName("Spot 6/GA Parking|A"), ),
+        7 : (CustomizedName("Spot 6/GA Parking|B"), ),
+        8 : (CustomizedName("Spot 6/GA Parking|C"), ),
+        9 : (CustomizedName("Spot 6/GA Parking|D"), ),
+        10 : (CustomizedName("Spot 6/GA Parking|H"), ),
+        11 : (CustomizedName("Spot 6/GA Parking|I"), ),
+        12 : (CustomizedName("Spot 6/GA Parking|J"), ),
+        13 : (CustomizedName("Spot 6/GA Parking|K"), ),
+        14 : (CustomizedName("Spot 6/GA Parking|L"), ),
+    }
+}
+
