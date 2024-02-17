ab84d0043ca7a16952ad26fb6fdf4f1852045222
Rokumaru <73397638+Rokumaru-JPN@users.noreply.github.com>
2024-02-17 14:14:09 +0900

    Version 1.0 Upload

---
+++ rjec-mfsg.py
@@ -0,0 +1,22 @@
+﻿msfs_mode = 1
+
+
+parkings = {
+    GATE : {
+        None : (),
+        1 : (CustomizedName("Apron Stand|Spot #§ "), ),
+        2 : (CustomizedName("Apron Gate|Spot #§ [JAL/JJP]"), ),
+        3 : (CustomizedName("Apron Gate|Spot #§ [JAL/JJP/ADO/ANA]"),),
+        4 : (CustomizedName("Apron Gate|Spot #§ [ADO/ANA/INTL]"),),
+        5 : (CustomizedName("Apron Gate|Spot #§ [INTL]"), ),
+    },
+    PARKING : {
+        None : (),
+        11 : (CustomizedName("GA Parking|Spot #§"), ),
+        12 : (CustomizedName("GA Parking|Spot #§"), ),
+        13 : (CustomizedName("GA Parking|Spot #§"), ),
+        14 : (CustomizedName("GA Parking|Spot #§"), ),
+        15 : (CustomizedName("GA Parking|Spot #§"), ),
+    }
+}
+
