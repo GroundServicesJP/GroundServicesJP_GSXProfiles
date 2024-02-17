5a654a33d12087063b4a9e231f8c70702af3a3bc
Rokumaru <73397638+Rokumaru-JPN@users.noreply.github.com>
2024-02-17 13:37:30 +0900

    Version 1.0 Upload

---
+++ rjth-karuchie.py
@@ -0,0 +1,20 @@
+﻿msfs_mode = 1
+
+parkings = {
+    GATE : {
+        None : (),
+        3 : (CustomizedName("Gate|Spot #§ [ANA]"), ),
+    },
+    PARKING : {
+        None : (),
+        1 : (CustomizedName("Stand|Spot #§"), ),
+        2 : (CustomizedName("Stand|Spot #§"), ),
+    },
+    0 : {
+        5 : (CustomizedName("GA Parking|#§"), ),
+        6 : (CustomizedName("GA Parking|#§"), ),
+        7 : (CustomizedName("GA Parking|#§"), ),
+        8 : (CustomizedName("GA Parking|#§"), ),
+        9 : (CustomizedName("GA Parking|#§"), ),
+    }
+}
