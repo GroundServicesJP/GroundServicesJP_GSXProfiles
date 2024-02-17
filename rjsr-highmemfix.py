50fa3f5c0bd150297fdd8ab2d096f1ccb4f37de3
Rokumaru <73397638+Rokumaru-JPN@users.noreply.github.com>
2024-02-17 13:43:19 +0900

    Version 1.0 upload

---
+++ rjsr-highmemfix.py
@@ -0,0 +1,16 @@
+﻿msfs_mode = 1
+
+
+parkings = {
+    GATE :{
+        None : (),
+        1 : (CustomizedName("Apron Stand|Spot #§"), ),
+        2 : (CustomizedName("Apron Gate|Spot #§ [ANA]"), ),
+        3 : (CustomizedName("Apron Stand|Spot #§"), ),
+        5 : (CustomizedName("Apron Stand|Spot #§"), ),
+    },
+    PARKING :{
+        None : (),
+        5 : (CustomizedName("GA Parking|#§"), ),
+    }
+}
