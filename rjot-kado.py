24917d04e43e35e9460b3955de1f4b6676a36e36
anaflightsim <66984894+anaflightsim@users.noreply.github.com>
2024-03-14 21:08:17 -0400

    Add files via upload

--- rjot-kado.py
+++ rjot-kado.py
@@ -30,11 +30,11 @@ def customOffsetGate_3(aircraftData):
     }
     table_787 = {
         8: -1.80,
-        9: 0,
-        10: 0,
+        9: 0.,
+        10: 0.,
     }
     if aircraftData.idMajor == 787:
-        return Distance.fromMeters(table_787.get(aircraftData.idMajor))
+        return Distance.fromMeters(table_787.get(aircraftData.idMinor, 0))
     else:
         try:
             return Distance.fromMeters(table.get(aircraftData.idMajor))
@@ -58,32 +58,32 @@ def customOffsetGate_5_6(aircraftData):
 parkings = {
     GATE : {
         None : (),
-        2 : (CustomizedName("TMNL GATE 2"), customOffsetGate_2),
-        3 : (CustomizedName("TMNL GATE 3"), customOffsetGate_3),
-        5 : (CustomizedName("TMNL GATE 5"), customOffsetGate_5_6),
-        6 : (CustomizedName("TMNL GATE 6"), customOffsetGate_5_6),
+        2 : (CustomizedName("Apron Gate|Spot 2 [JAL]"), customOffsetGate_2),
+        3 : (CustomizedName("Apron Gate|Spot 3 [ANA/JJP]"), customOffsetGate_3),
+        5 : (CustomizedName("Apron Gate|Spot 5 [ANA/JJP/INTL]"), customOffsetGate_5_6),
+        6 : (CustomizedName("Apron Gate|Spot 6 [ANA/INTL]"), customOffsetGate_5_6),
     },
     PARKING : {
         None : (),
-        1 : (CustomizedName("TMNL PARKING 1"), customOffsetGate_0),
-        7 : (CustomizedName("TMNL PARKING 7"), customOffsetGate_0),
-        12 : (CustomizedName("GA PARKING C"), customOffsetGate_0),
-        13 : (CustomizedName("GA PARKING E"), customOffsetGate_0),
-        14 : (CustomizedName("GA PARKING G"), customOffsetGate_0),
-        15 : (CustomizedName("GA PARKING F"), customOffsetGate_0),
-        16 : (CustomizedName("GA PARKING D"), customOffsetGate_0),
-        18 : (CustomizedName("GA PARKING K"), customOffsetGate_0),
-        19 : (CustomizedName("GA PARKING J"), customOffsetGate_0),
-        20 : (CustomizedName("GA PARKING L"), customOffsetGate_0),
-        21 : (CustomizedName("GA PARKING M"), customOffsetGate_0),
-        22 : (CustomizedName("GA PARKING N"), customOffsetGate_0),
-        23 : (CustomizedName("GA PARKING O"), customOffsetGate_0),
-        24 : (CustomizedName("GA PARKING P"), customOffsetGate_0),
-        31 : (CustomizedName("GA PARKING W"), customOffsetGate_0),
-        32 : (CustomizedName("GA PARKING X"), customOffsetGate_0),
-        33 : (CustomizedName("GA PARKING Y"), customOffsetGate_0),
-        34 : (CustomizedName("GA PARKING Z"), customOffsetGate_0),
-        35 : (CustomizedName("GA PARKING B"), customOffsetGate_0),
-        36 : (CustomizedName("GA PARKING A"), customOffsetGate_0),
+        1 : (CustomizedName("Apron Stand|Spot 1"), customOffsetGate_0),
+        7 : (CustomizedName("Apron Stand|Spot 7"), customOffsetGate_0),
+        12 : (CustomizedName("East Apron|C"), customOffsetGate_0),
+        13 : (CustomizedName("East Apron|E"), customOffsetGate_0),
+        14 : (CustomizedName("East Apron|G"), customOffsetGate_0),
+        15 : (CustomizedName("East Apron|F"), customOffsetGate_0),
+        16 : (CustomizedName("East Apron|D"), customOffsetGate_0),
+        18 : (CustomizedName("East Apron|K"), customOffsetGate_0),
+        19 : (CustomizedName("East Apron|J"), customOffsetGate_0),
+        20 : (CustomizedName("East Apron|L"), customOffsetGate_0),
+        21 : (CustomizedName("East Apron|M"), customOffsetGate_0),
+        22 : (CustomizedName("East Apron|N"), customOffsetGate_0),
+        23 : (CustomizedName("East Apron|O"), customOffsetGate_0),
+        24 : (CustomizedName("East Apron|P"), customOffsetGate_0),
+        31 : (CustomizedName("West Apron|W"), customOffsetGate_0),
+        32 : (CustomizedName("West Apron|X"), customOffsetGate_0),
+        33 : (CustomizedName("West Apron|Y"), customOffsetGate_0),
+        34 : (CustomizedName("West Apron|Z"), customOffsetGate_0),
+        35 : (CustomizedName("East Apron|B"), customOffsetGate_0),
+        36 : (CustomizedName("East Apron|A"), customOffsetGate_0),
     },
 }
