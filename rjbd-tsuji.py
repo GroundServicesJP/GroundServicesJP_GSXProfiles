93955b5af2263b893de1524af9660d6ec1fbe817
Rokumaru <73397638+Rokumaru-JPN@users.noreply.github.com>
2024-09-08 20:17:31 +0900

    Add files via upload
    Nanki-Shirahama Airport (RJBD)
    rjbd-tsuji.ini/.py

---
+++ rjbd-tsuji.py
@@ -0,0 +1,52 @@
+﻿# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+msfs_mode = 1
+
+@AlternativeStopPositions
+def customOffset_noOffset(aircraftData):
+    return Distance.fromMeters(0.)
+
+parkings = {
+    GATE : {
+        None : (),
+        1 : (CustomizedName("Apron (South)|SPOT #§"), customOffset_noOffset),
+        2 : (CustomizedName("Apron (South)|SPOT # {Gate}§"), customOffset_noOffset),
+        3 : (CustomizedName("Apron (South)|SPOT #§"), customOffset_noOffset),
+    }, 
+
+    N_PARKING : {
+        None : (),
+        1 : (CustomizedName("North Apron|1"), ),
+        2 : (CustomizedName("North Apron|2"), ),
+        3 : (CustomizedName("North Apron|3"), ),
+        4 : (CustomizedName("North Apron|4"), ),
+
+        6: (CustomizedName("North Apron|A"), ),
+        8: (CustomizedName("North Apron|C"), ),
+    }, 
+
+    S_PARKING : {
+        None : (),
+        1 : (CustomizedName("South Apron (GA)|A"), ),
+        2 : (CustomizedName("South Apron (GA)|B"), ),
+        4 : (CustomizedName("South Apron (GA)|D"), ),
+        5 : (CustomizedName("South Apron (GA)|E"), ),
+
+        6: (CustomizedName("South Apron (GA)|F"), ),
+        7: (CustomizedName("South Apron (GA)|G"), ),
+        8: (CustomizedName("South Apron (GA)|H"), ),
+        9: (CustomizedName("South Apron (GA)|I"), ),
+        10: (CustomizedName("South Apron (GA)|J"), ),
+    }, 
+}
