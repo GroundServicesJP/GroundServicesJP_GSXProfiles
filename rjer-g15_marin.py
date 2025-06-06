b5af18cc33671645c0052e19d4fbce40d87f8421
anaflightsim <66984894+anaflightsim@users.noreply.github.com>
2025-06-06 05:38:48 -0500

    Update RJER to v1.1

--- rjer-g15_marin.py
+++ rjer-g15_marin.py
@@ -1,17 +1,22 @@
-﻿# This program is free software: you can redistribute it and/or modify
+# -- coding: utf-8 --
+
+# Copyright (C) 2025 GroundServicesJP
+#
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 msfs_mode = 1
+version = 1.1
 
 @AlternativeStopPositions
 def customOffset_RJER(aircraftData):
@@ -28,9 +33,9 @@ def customOffset_RJER(aircraftData):
 parkings = {
     PARKING :{
         None : (),
-        1 : (CustomizedName("Apron|Spot #§"), customOffset_RJER),
-        2 : (CustomizedName("Apron|Spot #§ [ANA/HAC]"), customOffset_RJER),
-        3 : (CustomizedName("Apron|Spot #§"), ),
-        5 : (CustomizedName("Apron|Spot #§"), ),
+        1 : (CustomizedName("Apron|SPOT #§"), customOffset_RJER),
+        2 : (CustomizedName("Apron|SPOT #§ [ANA/HAC]"), customOffset_RJER),
+        3 : (CustomizedName("Apron|SPOT #§"), ),
+        5 : (CustomizedName("Apron|SPOT #§"), ),
     }
 }
